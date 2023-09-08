9c3ae8dfc6fdabfd8b1d9ad9b09f9107a83bd496
Eshaan Manchanda <eshaanmanchanda01@gmail.com>
2023-09-08 22:28:20 +0530

    first commit

---
+++ src/graph/bar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [{
+        label: "Revenue",
+        backgroundColor: "rgba(2,117,216,1)",
+        borderColor: "rgba(2,117,216,1)",
+        data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }],
+};
+function bar() {
+  return (
+    <Bar data={data}></Bar>
+  )
+}
+
+export default bar
---
+++ src/graph/pointChar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+  );
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  
+  
+  export const data = {
+    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    datasets: [
+      {
+        fill: true,
+        label: 'Dataset 2',
+        data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  
+function pointChar() {
+  return (
+    <Line options={options} data={data} />
+  )
+}
+
+export default pointChar
